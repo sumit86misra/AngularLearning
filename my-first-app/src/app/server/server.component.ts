03f3a31538eacaeb9a54219c8fa32753cea6b913
sumit86misra <shivam.misra@>
2019-04-26 07:26:14 +0530

    Initial checkin

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-server',
+    templateUrl:'./server.component.html'
+})
+export class ServerComponent{
+  serverId:number=10;
+  serverStatus:string='online'
+constructor(){
+  this.serverStatus=Math.random()>0.5?"online":"offline"
+}
+getServerStatus()
+{
+    return this.serverStatus;
+}
+}
